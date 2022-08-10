9da2d13feaa7d877aab4fe22a95420857f3cfdc1
Cera White <cwhite@anigramsproductions.com>
2022-08-09 17:34:18 -0700

    Add assets and drag-and-drop functionality

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="match-3-app">
-    <h1>Match 3</h1>
+    <header class="match-3-app__header">
+      <h1>Ophelia's Potion Shop</h1>
+    </header>
+    
+    <aside class="match-3-app__left">
+      <!-- Left -->
+    </aside>
 
-    <game-board></game-board>
+    <game-board class="match-3-app__main"></game-board>
+
+    <aside class="match-3-app__right">
+      <!-- Right -->
+    </aside>
+
+    <footer class="match-3-app__footer">
+      <p>&copy; Anigrams Productions 2022</p>
+    </footer>
   </div>
 </template>
 
@@ -18,4 +32,96 @@ export default {
 </script>
 
 <style>
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
+
+  img { 
+    display: block; 
+  }
+
+  body {
+    font-family: 'Quicksand', sans-serif;
+    background-image: url('~@/assets/bg/alchemist2.png');
+    background-repeat: no-repeat; /* Do not repeat the image */
+    background-size: cover; /* Resize the background image to cover the entire container */
+    color: #ffffff;
+    height: 100vh;
+  }
+
+  #app,
+  .match-3-app {
+    height: 100%;
+  }
+
+  h1 {
+    font-family: 'Indie Flower', cursive;
+    font-size: 54px;
+  }
+
+  .match-3-app {
+    padding: 20px;
+    display: grid;
+    grid-template-rows: auto auto auto 1fr auto;
+    gap: 20px;
+    grid-template-areas:
+      "header"
+      "leftside"
+      "rightside"
+      "main"
+      "footer";
+  }
+
+  .match-3-app__header {
+    grid-area: header;
+  }
+
+  .match-3-app__left {
+    grid-area: leftside;
+  }
+
+  .match-3-app__main {
+    grid-area: main;
+  }
+
+  .match-3-app__right {
+    grid-area: rightside;
+  }
+
+  .match-3-app__footer {
+    grid-area: footer;
+    text-align: center;
+  }
+
+  /* 2-column layout */
+  @media (min-width: 700px) {
+    .match-3-app {
+      grid-template-columns: 2fr 2fr;
+      grid-template-rows: auto auto 1fr auto;
+      grid-template-areas:
+        "header header"
+        "leftside rightside"
+        "main main"
+        "footer footer";
+    }
+  }
+
+    /* 3-column layout */
+  @media (min-width: 1000px) {
+    .match-3-app {
+      grid-template-columns: 1fr 3fr 1fr;
+      grid-template-rows: auto 1fr auto;
+      grid-template-areas:
+        "header header header"
+        "leftside main rightside"
+        "footer footer footer";
+    }
+  }
+
 </style>
--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="game-board">
-    <div class="game-board__row" v-for="row in rows" :key="`row-${row}`">
-      <div class="game-board__tile" v-for="tile in tiles.slice((row - 1) * columns, row * columns)" :key="`tile-r${tile.row}-c${tile.col}`">
-        <span>Row: {{ tile.row }}</span>
-        <span>Col: {{ tile.col }}</span>
-        <span>{{ tile.type }}</span>
+  <div class="game-board" ref="gameBoard">
+    <div class="game-board__wrapper">
+      <div class="game-board__row" v-for="row in rows" :key="`row-${row}`">
+        <div class="game-board__tile" :class="{ 'game-board__tile--active': tile.active }"
+          v-for="tile in tiles.slice((row - 1) * columns, row * columns)"
+          :key="tile.id"
+          :style="tileStyle"
+          @drop="onDrop($event, tile)"
+          @dragover.prevent
+          @dragenter.prevent
+        >
+          <span v-if="debug">
+            Row: {{ tile.row }} <br/>
+            Col: {{ tile.col }} <br/>
+            ID: {{ tile.id }}
+          </span>
+          <img class="game-board__tile-image" 
+            :src="require(`@/assets/tiles/${tile.type}.png`)" 
+            :alt="tile.type" 
+            draggable="true"
+            @dragstart="startDrag($event, tile)"
+            @dragend="endDrag($event, tile)" />
+        </div>
       </div>
     </div>
   </div>
@@ -25,43 +42,131 @@ export default {
     tileTypes: {
       type: Array,
       default() {
-        return ['heart', 'star', 'rainbow', 'horseshoe', 'balloon']
+        return ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
       }
+    },
+    debug: {
+      type: Boolean,
+      default: false
     }
   },
   data() {
     const tiles = []
+    let tileId = 0
 
     // for each row
     for (let r = 0; r < this.rows; r++) {
       for (let c = 0; c < this.columns; c++) {
         const tile = {
+          id: tileId,
           row: r,
           col: c,
-          type: this.tileTypes[Math.floor(Math.random() * this.tileTypes.length)]
+          type: this.tileTypes[Math.floor(Math.random() * this.tileTypes.length)],
+          active: false
         }
         tiles.push(tile)
+        tileId++
       }
     }
 
     return {
-      tiles
+      tiles,
+      tileSize: 5
     }
+  },
+  computed: {
+    tileStyle() {
+      return {
+        width: `${this.tileSize}px`,
+        height: `${this.tileSize}px`
+      };
+    }
+  },
+  methods: {
+    calculateTileSize() {
+      const boardSize = this.$refs.gameBoard.clientHeight - 24 // padding on game-board__wrapper x 2 (top & bottom)
+      const tileSize = Math.floor(boardSize / this.rows)
+
+      this.tileSize = tileSize
+    },
+    selectTile(tile) {
+      tile.active = true
+    },
+    unselectTile(tile) {
+      tile.active = false
+    },
+    startDrag(evt, tile) {
+      evt.dataTransfer.dropEffect = 'move'
+      evt.dataTransfer.effectAllowed = 'move'
+      evt.dataTransfer.setData('tileID', tile.id)
+      this.selectTile(tile)
+    },
+    endDrag(evt, tile) {
+      this.unselectTile(tile)
+    },
+    onDrop(evt, currentTile) {
+      const tileID = evt.dataTransfer.getData('tileID')
+      const newTile = this.tiles.find((tile) => tile.id == tileID)
+
+      // only allow swapping of tiles next to each other
+      if (Math.abs(newTile.col - currentTile.col) > 1) {
+        // trying to move more than 1 space
+        return
+      } else if (Math.abs(newTile.row - currentTile.row) > 1) {
+        // trying to move more than 1 space
+        return
+      } else if (newTile.col != currentTile.col && newTile.row != currentTile.row) {
+        // trying to move diagonally
+        return
+      }
+
+      // TODO: check if swap is possible
+
+      const newType = newTile.type
+      const currentType = currentTile.type
+
+      currentTile.type = newType
+      newTile.type = currentType
+    },
+  },
+  mounted() {
+    this.calculateTileSize()
   }
 }
 </script>
 
 <style scoped>
+  .game-board {
+    text-align: center;
+  }
+
+  .game-board__wrapper {
+    background: rgba(0,0,0,0.8);
+    border-radius: 12px;
+    display: inline-block;
+    padding: 12px;
+  }
+
+  .game-board__row {
+    line-height: 0;
+  }
+
   .game-board__tile {
     display: inline-block;
-    width: 100px;
-    height: 100px;
-    border: 1px solid black;
+    line-height: 0;
+    padding: 4px;
+    position: relative;
+  }
+
+  .game-board__tile-image {
+    width: 100%;
+    height: 100%;
   }
 
   .game-board__tile span {
+    position: absolute;
     display: block;
-    margin: 0;
-    padding: 0;
+    font-size: 12px;
+    line-height: normal;
   }
 </style>
